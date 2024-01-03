8d31e2d719d7911c66e7734a315bb7b5d4024f3c
AdamS <surzyn.adam@gmail.com>
2024-01-03 20:46:43 +0100

    Adding the beggining of Context API excercise 1

---
+++ context-api-ex1/contexts/notificationContext.tsx
@@ -0,0 +1,50 @@
+import {
+  useContext,
+  useState,
+  createContext,
+  Dispatch,
+  SetStateAction,
+} from "react";
+import React from "react";
+
+type Action = "INCREMENT" | "DECREMENT";
+type ActionType = {
+  type: Action;
+};
+type State = {
+  count: number;
+};
+type Notification = {
+  type: "added" | "removed" | "edited";
+  read: boolean;
+};
+type Notifications = Notification[];
+
+const initialState = { count: 0 };
+const actionTypes = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT",
+};
+
+const reducer = (state: State, action: ActionType): State => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+/**
+ *
+ * Plan jest taki, ze:
+ * Reducer bedzie uzyty przy przyciskach w komponencie 'profiles'
+ * Po kazdej zmianie bedzie szedl api call, zalezny od akcji do json-server
+ * Przy kazdym dodaniu/edycji/usunieciu profilu, do contextu bedzie dodawane, ile jest zmian i jaka byla ostatnia zmiana() i jej czas
+ * Context wyswietlany jako koleczko w ktoryms rogu, o kolorze zaleznie od zmiany, majac w srodku liczbe zmian. Po kliknieciu pokazuje sie czas zmiany w modalu. Zeby trackowac wszystkie zmiany, musze je trzymac w jakims arrayu.
+ * !Czy jest opcja robienia api calla, przy zmianie aktualnej strony? W ten sposob moglbym trzymac informacje o aktualnych profilach w jakims arrayu
+ * !i robic call w momencie, w ktorym jest to potrzebne i mam potencjalnie pare zmian do wprowadzenia na raz. Moglbym uzyc do tego stanu, trackujacego url albo cos. Ew za bardzo kombinuje xd
+ *
+ */
